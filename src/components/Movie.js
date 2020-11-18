240e20f9d911b365da13242a72a5521e1ddd2ea9
kimsupport <theipadslayer@gmail.com>
2020-11-18 12:19:19 +0900

    1118
    일단은 끗 오늘 다시 분석해봅시다.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
-function Movie({year, title, summary, poster, genres}) {
-    return <div className="movie">
+function Movie({ id, year, title, summary, poster, genres}) {
+    return( 
+    <div className="movie">
+      <Link
+        to={{
+            pathname: `movie/${id}`,
+            state:{
+                year:year,
+                title:title,
+                summary,
+                poster,
+                genres
+            }
+        }}
+      >  
         <img src={poster} alt={title} title={title}></img>
         <div className="movie_data">
             <h3 className="movie_title">{title}</h3>
@@ -14,9 +28,11 @@ function Movie({year, title, summary, poster, genres}) {
                         {genre}
                     </li>)}
             </ul>
-            <p className="movie_summary">{summary}</p>
+            <p className="movie_summary">{summary.slice(0, 180)}...</p>
         </div>            
+        </Link>
     </div>
+    )
 }
 
 Movie.propTypes = {
---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./About.css"
+
+function About(props){
+    console.log(props);
+    return (
+        <div className="about_container">
+            <span>
+                "Who the Fuck boies hang out On the Net"
+            </span>
+            <span>Kidmili</span>
+        </div>
+    )
+}
+
+export default About;
